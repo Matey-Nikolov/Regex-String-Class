ea919d417923281a1b2ec1fdc50b9e43669ff515
Matey-Nikolov <matey.nikolov@mail.bg>
2021-05-17 17:31:19 +0300

    New tsak.

---
+++ NameHealthDamage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_Class_String
+{
+    class NameHealthDamage
+    {
+        public string Name { get; set; }
+
+        public int Health { get; set; }
+
+        public double Damage { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -42,13 +42,81 @@
                         Regex_Task studentGrade = new Regex_Task();
                         studentGrade.RegexStudentGrade();
                         break;
-                   // case 9: RegexTask2(); break;
+                    case 9:  NetherRealms(); break;
                     case 10: return;
 
                 }
                 Console.WriteLine("----------");
                 Console.Write("Enter a num 1 - 10 : ");
                 n = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public static void NetherRealms()
+        {
+                        //Dictionary<string, Dictionary<int, double>> nameHealthDamage = new Dictionary<string, Dictionary<int, double>>();
+
+            List<NameHealthDamage> nameHealthDamages = new List<NameHealthDamage>();
+
+            List<string> codeText = Console.ReadLine()
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            int health = 0;
+            double damage = 0;
+
+            foreach (string text in codeText)
+            {
+
+                string patternWordHealth = @"[A-za-z]+";
+                MatchCollection codeWordMatch = Regex.Matches(text, patternWordHealth);
+
+                string letter = "";
+                foreach (var chr in codeWordMatch)
+                    letter += chr.ToString();
+
+                foreach (char chr in letter)
+                    health += (int)(chr);
+
+                string patternDamage = @"[-?\d.?]+";
+                MatchCollection codeNumbers = Regex.Matches(text, patternDamage);
+
+                foreach (var digit in codeNumbers)
+                    damage += double.Parse(digit.ToString());
+
+                string paternMultiaOrDivision = @"[\/|*]+";
+                MatchCollection MultiAndDivision = Regex.Matches(text, paternMultiaOrDivision);
+
+                foreach (var item1 in MultiAndDivision)
+                {
+                    foreach (char item2 in item1.ToString())
+                    {
+                        if (item2 == '*')
+                        {
+                            damage *= 2;
+                        }
+                        else if (item2 == '/')
+                        {
+                            damage /= 2;
+                        }
+                    }
+                }
+
+                nameHealthDamages.Add(new NameHealthDamage()
+                {
+                    Name = text,
+                    Health = health,
+                    Damage = damage
+                });
+
+                health = 0;
+                damage = 0;
+            }
+
+            foreach (var NHD in nameHealthDamages
+                .OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{NHD.Name} - {NHD.Health} health, {NHD.Damage:f2} damage");
             }
         }
 
